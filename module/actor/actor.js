b09f9eaddb13a135d71837921280810aa9ef6f39
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-02-24 17:36:41 -0600

    Added the ability to get HP/MP/IP from classes. I also added the initial templates for several types of item.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -16,28 +16,33 @@ export class FabulaUltimaActor extends Actor {
     _preparePlayerData(actorData) {
         const { system } = actorData;
 
+        //Reset bonuses in case a class was removed
+        system.hp.bonus = 0;
+        system.mp.bonus = 0;
+        system.ip.bonus = 0;
+
         //Calculate statistics
         for (let [key, statistic] of Object.entries(system.attributes)) {
             statistic.current = clamp(statistic.base + statistic.bonus, 6, 12);
         }
 
-        // Calculate the character's level
+        // Calculate class-based stats
         let _tempLevel = 0;
 
         for (let [key, klass] of Object.entries(system.classes)) {
+            system.hp.bonus += klass.benefits.resource.hp;
+            system.mp.bonus += klass.benefits.resource.mp;
+            system.ip.bonus += klass.benefits.resource.ip;
+
             _tempLevel += klass.level;
         }
         system.characterLevel = clamp(_tempLevel, 5, 50);
 
         // Update resources to reflect bonuses
         system.hp.max =
-            system.attributes.might.current * 5 +
-            system.characterLevel +
-            system.hp.bonus;
+            system.attributes.might.current * 5 + system.characterLevel + system.hp.bonus;
         system.mp.max =
-            system.attributes.willpower.current * 5 +
-            system.characterLevel +
-            system.mp.bonus;
+            system.attributes.willpower.current * 5 + system.characterLevel + system.mp.bonus;
         system.ip.max = 6 + system.ip.bonus;
 
         system.hp.value = clamp(system.hp.value, 0, system.hp.max);
