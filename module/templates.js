ae5735992e6775e2a2fa47ba78aedfe81be1b1b7
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-02-25 17:38:08 -0600

    Moved the existing partials into a directory. This structure will be much better for organization.

--- module/templates.js
+++ module/templates.js
@@ -1,8 +1,8 @@
 export const preloadHandlebarsTemplates = async function () {
     return loadTemplates([
-        "systems/fabulaultima/templates/actor/class-card.hbs",
-        "systems/fabulaultima/templates/actor/attribute-dropdown.hbs",
-        "systems/fabulaultima/templates/actor/resource-card.hbs",
+        "systems/fabulaultima/templates/actor/partials/class-card.hbs",
+        "systems/fabulaultima/templates/actor/partials/attribute-dropdown.hbs",
+        "systems/fabulaultima/templates/actor/partials/resource-card.hbs",
         "systems/fabulaultima/templates/item/partials/ritual-row.hbs",
     ]);
 };
