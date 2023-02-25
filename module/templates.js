fe010828b7f33c7176222b66b5c8585d8148a299
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-02-25 17:28:26 -0600

    Moved the ritual checkboxes to a partial.

--- module/templates.js
+++ module/templates.js
@@ -3,5 +3,6 @@ export const preloadHandlebarsTemplates = async function () {
         "systems/fabulaultima/templates/actor/class-card.hbs",
         "systems/fabulaultima/templates/actor/attribute-dropdown.hbs",
         "systems/fabulaultima/templates/actor/resource-card.hbs",
+        "systems/fabulaultima/templates/item/partials/ritual-row.hbs",
     ]);
 };
