57454878cf75250637088727f70fdabd74b8746c
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-02-27 20:53:37 -0600

    Initial implementation of weapon-type items. Most of the basic information is available to configure. Still working on layout and equipping it to the player.

--- module/templates.js
+++ module/templates.js
@@ -4,5 +4,7 @@ export const preloadHandlebarsTemplates = async function () {
         "systems/fabulaultima/templates/actor/partials/attribute-dropdown.hbs",
         "systems/fabulaultima/templates/actor/partials/resource-card.hbs",
         "systems/fabulaultima/templates/item/partials/ritual-row.hbs",
+        "systems/fabulaultima/templates/item/partials/attribute-dropdown.hbs",
+        "systems/fabulaultima/templates/item/partials/damage-type-dropdown.hbs",
     ]);
 };
