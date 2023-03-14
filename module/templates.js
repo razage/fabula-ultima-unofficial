c9bd021487c02229236113e3fd3a0be5d3c24a4e
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-03-13 20:09:57 -0500

    Finished layout for the accessory card

--- module/templates.js
+++ module/templates.js
@@ -10,5 +10,6 @@ export const preloadHandlebarsTemplates = async function () {
         "systems/fabulaultima/templates/actor/partials/resistance-dropdown.hbs",
         "systems/fabulaultima/templates/actor/partials/bond-card.hbs",
         "systems/fabulaultima/templates/actor/partials/armor-card.hbs",
+        "systems/fabulaultima/templates/actor/partials/accessory-card.hbs",
     ]);
 };
