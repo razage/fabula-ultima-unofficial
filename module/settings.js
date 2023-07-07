4f02089147852350fea5b7db2ec8789de494a79a
Thomas Hinkle <thomashinkle2393@gmail.com>
2023-07-07 11:56:54 -0500

    added text clarifying how the color settings work.

--- module/settings.js
+++ module/settings.js
@@ -1,7 +1,7 @@
 export const registerSystemSettings = function () {
     game.settings.register("fabulaultima", "backgroundColor1", {
         name: "Background Color 1",
-        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #. It forms a gradient from this color to the next one.",
+        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #. It forms a gradient from this color to the next one. You will have to return to setup and relaunch this system to see the change.",
         scope: "world", // This specifies a world-stored setting
         config: true, // This specifies that the setting appears in the configuration view
         type: String,
@@ -10,7 +10,7 @@ export const registerSystemSettings = function () {
 
     game.settings.register("fabulaultima", "backgroundColor2", {
         name: "Background Color 2",
-        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #. It forms a gradient from the previous color to this one.",
+        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #. It forms a gradient from the previous color to this one. You will have to return to setup and relaunch this system to see the change.",
         scope: "world",
         config: true,
         type: String,
@@ -19,7 +19,7 @@ export const registerSystemSettings = function () {
 
     game.settings.register("fabulaultima", "fontColor", {
         name: "Font Color",
-        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #.",
+        hint: "Use a color picker and get the hex code for the color you want. Don't forget the #. You will have to return to setup and relaunch this system to see the change.",
         scope: "world",
         config: true,
         type: String,
